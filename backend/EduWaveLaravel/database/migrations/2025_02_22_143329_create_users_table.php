8503f8ae752f97999eee34b9f15246ac7d9f3ba7
jdu211171 <211171m@jdu.uz>
2025-03-05 07:51:40 +0500

    users table updated

--- backend/EduWaveLaravel/database/migrations/2025_02_22_143329_create_users_table.php
+++ backend/EduWaveLaravel/database/migrations/2025_02_22_143329_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name')->nullable()->change();
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('unique_id')->unique()->nullable();
             $table->string('password');
